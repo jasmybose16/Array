fd9fdc54088b1c278833b56c3aac3ecd17b56ded
Jasmy Bose <115951078+jasmybose@users.noreply.github.com>
2024-05-02 14:35:05 +0100

    multi dimensional arrays

---
+++ multiarray.java
@@ -0,0 +1,13 @@
+public class multiarray {
+    public static void main(String[] args) {
+        int arr[][]=new int[2][4];
+         for (int i=0;i<2;i++){
+            for(int j=0;j<4;j++){
+                System.out.print(arr[i][j] +"");
+            }
+            System.out.println();
+
+         }        
+    }
+    
+}
