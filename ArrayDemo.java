483a9ed95cc2280a7e46ac8f59652ef43f4b9dec
Jasmy Bose <115951078+jasmybose@users.noreply.github.com>
2024-05-02 10:25:11 +0100

    Array demo

---
+++ ArrayDemo.java
@@ -0,0 +1,9 @@
+class ArrayDemo{
+    public static void main(String[] args) {
+        int arr[]={1,2,3,4};
+        arr[2]=5;
+        
+        System.out.println(arr[2]);
+        System.out.println(arr);
+    }
+}
