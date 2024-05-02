3564f25d916cfed955932a4d8f7f26a338398743
Jasmy Bose <115951078+jasmybose@users.noreply.github.com>
2024-05-02 10:53:50 +0100

    array modified

--- ArrayDemo.java
+++ ArrayDemo.java
@@ -2,8 +2,17 @@ class ArrayDemo{
     public static void main(String[] args) {
         int arr[]={1,2,3,4};
         arr[2]=5;
-        
-        System.out.println(arr[2]);
-        System.out.println(arr);
+        int num[]=new int[4];
+        num[0]=2;
+        num[1]=5;
+        num[2]=7;
+        num[3]=8;
+
+       for(int i=0;i<=3;i++) {
+        System.out.println(num[i]);
+       }
+       // System.out.println(arr[2]);
+        //System.out.println(arr);
     }
+    
 }
